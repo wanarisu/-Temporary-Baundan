fbc854144feca7380fb114d6450b3d109619802f
wanarisu <wanarisu@gmail.com>
2019-04-03 12:51:49 +0900

    初期設定完了

---
+++ バウンダン/V1/Assets/Script/Kubota/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+	public float speed;
+
+	private new Rigidbody rigidbody;
+	private Vector3 vector3;
+
+	private const float number_Of_Directions = 1.0f;
+
+	void Start () 
+	{
+		vector3 = Vector3.zero;
+
+		rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = vector3;
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+			vector3.z = number_Of_Directions;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+			vector3.z = -number_Of_Directions;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+			vector3.x = number_Of_Directions;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 270.0f, 0.0f);
+			vector3.x = -number_Of_Directions;
+		}
+		else
+		{
+			vector3 = Vector3.zero;
+		}
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+		}
+
+		rigidbody.velocity = vector3 * speed;
+	}
+
+	public void OnCollisionEnter(Collision col)
+	{
+		
+	}
+}
---
+++ バウンダン/V1/Assets/Script/Morooka/Sphere.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+	private new Rigidbody rigidbody;
+
+	public float xMax = 0.5f;
+	public float zMax = 1.0f;
+
+	private Vector3 progressVector_F;     //　進行ベクトル
+
+	public float speed;
+
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		progressVector_F = new Vector3(xMax, 0, zMax);
+		rigidbody.velocity = progressVector_F.normalized * speed;
+	}
+
+	void Update ()
+	{
+		//transform.position += progressVector_F;
+	}
+
+	void ReflectionVector()
+	{
+	}
+
+	public void OnCollisionEnter(Collision col)
+	{
+		Vector3 normalVector_N = Vector3.zero;
+		Vector3 vecocity = Vector3.zero;
+
+		normalVector_N = col.contacts[0].normal;
+
+		vecocity = progressVector_F + (2 * Vector3.Dot(-progressVector_F, normalVector_N) * normalVector_N);
+		progressVector_F = vecocity;
+		rigidbody.velocity = vecocity.normalized * speed;
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		
+	}
+
+	//　スカラー積の計算
+	private Vector3 ScalarProductCalculation(Vector3 a, Vector3 b)
+	{
+		a.x *= b.x;
+		a.y *= b.y;
+		a.z *= b.z;
+
+		return a;
+	}
+}
